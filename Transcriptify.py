f8ff78fcaa47aefc38d91b48bbb6a1fb84ce38f4
jfm421 <121835242+jfm421@users.noreply.github.com>
2023-10-05 09:09:53 +1100

    Update Transcriptify.py

--- Transcriptify.py
+++ Transcriptify.py
@@ -33,36 +33,18 @@ def summarize_text(chunked_text, model, progress_bar):
         progress_bar.progress((i+1)/len(chunked_text)*0.5)  # 50% of the progress bar for this task
     return " ".join(summaries)
 
-def generate_toc_content(summary, model, progress_bar):
-    default_sections = {
-        "Introduction": "Provide an introduction based on the summary.",
-        "Overview of the project and its objectives": "Extract the overview of the project and its main objectives from the summary.",
-        "Research Plan Overview": "Describe the research plan and its origin from initial workshops.",
-        "Key hypotheses articulated for the study": "Identify the key hypotheses that were articulated for the study from the summary.",
-        "Key Findings and Themes": "Extract the main findings and themes from the summary.",
-        "User-suggested remedies and potential solutions": "Identify user-suggested remedies and potential AI-related solutions from the summary.",
-        "Discussion on collective thoughts and approaches": "Highlight the collective thoughts and approaches from the summary.",
-        "Recommendations": "List the six to eight major themes from user feedback and potential solutions for each problem area, including suggestions for automation.",
-        "Personas and Sales Journey Mapping": "Provide an outline of personas, their goals, challenges, and opportunities, and jobs-to-be-done associated with each role from the summary.",
-        "Staff Roles Involved in the Sales Journey": "Mention the roles involved in the sales journey including Customer Solutions Consultant, Sales Operations Manager, Sales Support Manager, Product Manager, Account Manager, and Customer Relationship Manager.",
-        "Dimensions for Sales Journey Visualisation": "Discuss customer experience highs and lows, data source types, problem statements, tools used, and potential solutions across journey stages.",
-        "Map Governance and Update Process opportunities": "Recommend strategies for maintaining wikis, integrating feedback, and using competitor data.",
-        "User Interface (UI) Prototypes": "Describe preliminary designs for updated Confluence pages and other interfaces.",
-        "Conclusion and Next Steps": "Provide a recap of findings and the suggested path forward, mentioning upcoming presentations and deliverables.",
-        "Appendix": "Provide details on the original research plan document, detailed interview transcripts, and syntheses."
+def extract_themes_goals_challenges_painpoints(summary, model, progress_bar):
+    prompts = {
+        "themes": "Identify the key themes from the summary:",
+        "goals": "List the main goals mentioned in the summary:",
+        "challenges": "Highlight the main challenges from the summary:",
+        "pain points": "Point out the pain points based on the summary:"
     }
 
-    # Allow the user to customize prompts
-    sections = {}
-    for section, default_prompt in default_sections.items():
-        custom_prompt = st.text_input(f"Customize the prompt for '{section}':", default_prompt)
-        sections[section] = custom_prompt
+    results = {}
+    keys = list(prompts.keys())
 
-    content = {}
-    keys = list(sections.keys())
-    progress_increment = 1.0 / len(keys)
-
-    for i, (section, prompt) in enumerate(sections.items()):
+    for i, (key, prompt) in enumerate(prompts.items()):
         messages = [{"role": "user", "content": f"{prompt} {summary}"}]
         
         response = openai.ChatCompletion.create(
@@ -70,10 +52,10 @@ def generate_toc_content(summary, model, progress_bar):
             messages=messages
         )
         
-        content[section] = response.choices[0].message['content'].strip()
-        progress_bar.progress((i+1) * progress_increment)
+        results[key] = response.choices[0].message['content'].strip()
+        progress_bar.progress(0.5 + (i+1)/len(keys)*0.5)  # Remaining 50% of the progress bar
 
-    return content
+    return results
 
 def main():
     st.title('Transcript Analyzer')
@@ -81,15 +63,11 @@ def main():
     # Model switcher
     model_choice = st.selectbox(
         "Choose the model:", 
-        ["gpt-4", "gpt-4-32k", "gpt-3.5-turbo"]
+        ["gpt-4", "gpt-3.5-turbo"]
     )
-
+    
     uploaded_file = st.file_uploader("Upload a transcript text file", type=["txt"])
-
-    # Initialize these as None so they can be checked before being used
-    summarized_transcript = None
-    progress_bar = None
-
+    
     if uploaded_file is not None:
         transcript = uploaded_file.read().decode()
         
@@ -97,18 +75,16 @@ def main():
         progress_bar = st.progress(0)
         
         chunked_transcript = chunk_text(transcript)
+        
         summarized_transcript = summarize_text(chunked_transcript, model_choice, progress_bar)
+        extracted_info = extract_themes_goals_challenges_painpoints(summarized_transcript, model_choice, progress_bar)
         
         st.subheader("Summarized Transcript")
         st.write(summarized_transcript)
-
-    # This button should appear only if there's a summarized transcript to work with
-    if summarized_transcript and st.button("Customize Prompts"):
-        toc_content = generate_toc_content(summarized_transcript, model_choice, progress_bar)
-
-        for section, text in toc_content.items():
-            st.subheader(section)
-            st.write(text)
+        
+        for key, value in extracted_info.items():
+            st.subheader(key.capitalize())
+            st.write(value)
 
 if __name__ == "__main__":
     main()
