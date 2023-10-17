6458752cae74a9d85005755efbf1ebb8c36bf4cf
jfm421 <121835242+jfm421@users.noreply.github.com>
2023-10-17 16:42:48 +1100

    Update Transcriptify.py

--- Transcriptify.py
+++ Transcriptify.py
@@ -32,13 +32,13 @@ def summarize_text(chunked_text, model, progress_bar):
         progress_bar.progress((i+1)/len(chunked_text)*0.5)  # 50% of the progress bar for this task
     return " ".join(summaries)
 
-def extract_themes_goals_challenges_painpoints(summary, model, progress_bar):
+def extract_themes_information_stakeholders_goals_challenges_opportunities_capabilities(summary, model, progress_bar):
     prompts = {
         "themes": "Identify the key themes from the summary:",
-        "information concepts": "Identify and list the key information concepts and business objects mentioned in the summary",
+        "information": "Identify and list the key information concepts and business objects mentioned in the summary",
         "stakeholders: "Identify the stakeholders and organisations mentioned in the summary:",
         "goals": "List the main goals mentioned in the summary:",
-        "pain points and challenges": "Point out the pain points and challenges based on the summary:"
+        "challenges": "Point out the pain points and challenges based on the summary:"
         "opportunities": "Highlight the opportunities to resolve the pain points from the summary and provide a set of recommendations:",
         "capabilities": "Where possible infer connections and relate the above datapoints to relevant business capabilities in the bizbok style and provide succinct commentary on the following capability dimensions including human resources - roles and skills, business processes, information and data and technology enablers:", 
     }
@@ -79,7 +79,7 @@ def main():
         chunked_transcript = chunk_text(transcript)
         
         summarized_transcript = summarize_text(chunked_transcript, model_choice, progress_bar)
-        extracted_info = extract_themes_goals_challenges_painpoints(summarized_transcript, model_choice, progress_bar)
+        extracted_info = extract_themes_information_stakeholders_goals_challenges_opportunities_capabilities(summarized_transcript, model_choice, progress_bar)
         
         st.subheader("Summarized Transcript")
         st.write(summarized_transcript)
